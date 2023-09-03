95210f479d0c2e4f42c3f199e378adfda43151a6
Davendra Jayasingam <hello@davendra.me>
2023-09-04 01:44:28 +0800

    second test

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import GenreFilter from '@/components/filters/GenreFilter'
 import PaginatedList from '@/components/PaginatedList'
 import RatingsFilter from '@/components/filters/RatingsFilter'
 import Sorter from '@/components/filters/Sorter'
+import { getAbsolutePath } from '@/utils/helpers/absolutePathHelper'
 
 type Props = {
   searchParams: SearchParams
@@ -16,17 +17,23 @@ export default async function Home({ searchParams }: Props)
 
   const currentPage = 1
 
-  const [movies, genres] = await Promise.all([
+  const [movies, genres, test] = await Promise.all([
     getMovies({
       page: currentPage,
       sortedBy: searchParams.s !== undefined ? searchParams.s : 'pd',
       genreIds: Array.isArray(searchParams.g) ? searchParams.g.map(g => Number(g)) : searchParams.g !== undefined ? [Number(searchParams.g)] : [],
       rating: searchParams.r !== undefined ? Number(searchParams.r) : 0
     }),
-    getGenres()
+    getGenres(),
+    fetch(getAbsolutePath('/api/discover'), {
+      method: 'GET',
+      next: { revalidate: 20 }
+    }).then(res => res.json())
   ])
 
   return <div>
+
+    <p className='text-white'>Test result: {test}</p>
 
     <GenreFilter
       genres={genres}
